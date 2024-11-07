3720e4fe27cf4f83f7b586b60af416c50391058e
olesyavelikorodnaya <olesyavelikor@gmail.com>
2024-11-07 16:15:43 +0300

    add header

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import {LoadingIndicatorComponent} from "../loading-indicator/loading-indicator.component";
 import {RouterLink, RouterOutlet} from "@angular/router";
-
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [LoadingIndicatorComponent, RouterLink, RouterOutlet],
+  imports: [LoadingIndicatorComponent, RouterLink, RouterOutlet, HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
